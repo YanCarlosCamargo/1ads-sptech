6f3c1518be8e54202196aa007f594f8ab4b852c7
Yan Carlos Camargo <73352444+YanCarlosCamargo@users.noreply.github.com>
2023-03-07 22:40:05 -0300

    Aula 07/03
    Vimos CSS inline, interno e externo. Adicionei também algumas animações com keyframes.

---
+++ script.js
@@ -0,0 +1,20 @@
+var cardLaranja = document.querySelector('.card');
+var cardAzul = document.querySelector('.card-azul');
+var cardVermelho = document.querySelector('.card-vermelho');
+var cardCinza = document.querySelector('.card-cinza');
+
+cardLaranja.addEventListener('click', () => {
+    alert("Este é um card Laranja")
+})
+
+cardVermelho.addEventListener('click', () => {
+    alert("Este é um card Vermelho")
+})
+
+cardAzul.addEventListener('click', () => {
+    alert("Este é um card Azul")
+})
+
+cardCinza.addEventListener('click', () => {
+    alert("Este é um card Cinza")
+})
